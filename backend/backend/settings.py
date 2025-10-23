00ca78bc64d9a32f974138388417b9022c4997a3
Tomasz Jarko <tomasz.jarko@gmail.com>
2025-10-23 19:49:40 +0200

    Updating security patches

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -11,19 +11,19 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import load_dotenv
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
+load_dotenv()
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-e&ii($=u&es%m7xj7nfc*_r=lqev_(n0_ey=t^yh5!)243p=k!'
+SECRET_KEY = os.getenv("DJANGO_SECRET_KEY", "django-insecure-e&ii($=u&es%m7xj7nfc*_r=lqev_(n0_ey=t^yh5!)243p=k!")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv("DJANGO_DEBUG", "True") == "True"
 
 ALLOWED_HOSTS = []
 CORS_ALLOW_ALL_ORIGINS = True
